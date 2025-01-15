f794b2baa9343f83bf86c7be6d8edf447e6e210a
so17 <manesorina@gmail.com>
2025-01-15 16:23:14 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String inputFileXML="patientsLog.xml";
+        String outputFileXML="result.xml";
+
+        PatientReader reader=new PatientReader(inputFileXML,outputFileXML);
+        List<PatientLog> patientLogList=reader.parse(inputFileXML);
+
+        patientLogList.forEach(System.out::println);
 
     }
 }
